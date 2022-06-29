c79439cacd26cabab36359a9c461b0c0a71467fd
aburtsev1 <aburtsev@modeln.com>
2022-06-30 01:17:24 +0300

    added api and spec, updated spec
    Api tests
    1) Introduced new api for addBooks in BookRestController
    2) Updated Open Api spec
    3) Added API test for new Api addBooks

--- src/main/java/com/xpinjection/library/adaptors/api/BookRestController.java
+++ src/main/java/com/xpinjection/library/adaptors/api/BookRestController.java
@@ -2,16 +2,16 @@ package com.xpinjection.library.adaptors.api;
 
 import com.xpinjection.library.service.BookService;
 import com.xpinjection.library.service.dto.BookDto;
+import com.xpinjection.library.service.dto.Books;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotBlank;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Alimenkou Mikalai
@@ -27,4 +27,11 @@ public class BookRestController {
     List<BookDto> findBooksByAuthor(@RequestParam @NotBlank String author) {
         return bookService.findBooksByAuthor(author);
     }
+
+    @PostMapping(path = "/books", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    List<BookDto> addBooks(@RequestBody Map<String, String> books)  {
+        return bookService.addBooks(Books.fromMap(books));
+    }
+
 }
--- src/test/java/com/xpinjection/library/adaptors/api/BookApiTest.java
+++ src/test/java/com/xpinjection/library/adaptors/api/BookApiTest.java
@@ -2,9 +2,17 @@ package com.xpinjection.library.adaptors.api;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.SeedStrategy;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -24,5 +32,29 @@ public class BookApiTest extends AbstractApiTest {
             .body("size()", greaterThanOrEqualTo(1))
             .body("author", everyItem(equalTo("Craig Walls")))
             .body("name", contains("Spring in Action"));
+    }
+
+    @Test
+    @DataSet(value = "default-books.xml", strategy = SeedStrategy.REFRESH)
+    void ifBooksAreAddedTheyAreAllReturned() throws JSONException {
+        JSONObject jsonObj = new JSONObject()
+                .put("Spring in Action", "Craig Walls")
+                .put("Hibernate in Action", "Who cares?");
+        Response response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(jsonObj.toString())
+                .when()
+                .post("/books");
+        JsonPath jp = new JsonPath(response.asString());
+        String nameValues = jp.get("name").toString();
+        String authorValues = jp.get("author").toString();
+
+        response
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("size()", equalTo(2));
+        Assert.assertEquals("[Hibernate in Action, Spring in Action]", nameValues);
+        Assert.assertEquals("[Who cares?, Craig Walls]", authorValues);
     }
 }
